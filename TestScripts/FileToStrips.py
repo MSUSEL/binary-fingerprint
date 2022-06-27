98a24fcf7da11f6352d470920cc94e42cf44c1a1
RnMor777 <ryan.morganti@trojans.dsu.edu>
2022-06-27 15:47:27 -0600

    Separated proper RGB script from the one that creates red-yellow bands

--- TestScripts/FileToStrips.py
+++ TestScripts/FileToStrips.py
@@ -1,8 +1,8 @@
 import os
-from PIL import Image
+from PIL import Image, ImageEnhance
 import numpy as np
 
-width_table=[(10,94),(30,192),(60,384),(100,768),(200,1152),(500,1536),(1000,2304)]
+width_table=[(10,99),(30,189),(60,399),(100,756),(200,1155),(500,1533),(1000,2373)]
 
 path = "/home/ryan/MalFiles/PEFiles/"
 name = "ShinoLocker.bin"
@@ -21,13 +21,10 @@ with open(filepath, "rb") as f:
     while byte := f.read(image_width):
         image_array.append(list(byte))
 
-image_array[-1] += [0 for x in range(image_width-len(image_array[-1]))]
+image_array[-1] += [b"\x00" for x in range(image_width-len(image_array[-1]))]
 color_array = [[tuple(y[x:x+3]) for x in range(0, image_width, 3)] for y in image_array]
 
-h = len(color_array)
-w = int(image_width/3)
-arr = np.zeros([h, w, 3], dtype=np.uint8)
-for i in range(h):
-    for j in range(w):
-        arr[i,j] = color_array[i][j]
-Image.fromarray(arr).save(name+"color.png")
+np_array = np.array(color_array)
+data = Image.fromarray(np_array, mode="RGB")
+data2 = ImageEnhance.Brightness(data)
+data2.enhance(3.0).save(name+"banded.png")
