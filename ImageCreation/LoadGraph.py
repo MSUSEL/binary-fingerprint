e2fad15d6151f731b24684742624be76e92c6258
RnMor777 <ryan.morganti@trojans.dsu.edu>
2022-07-16 18:29:32 -0600

    Added program to view graph from pickled graph data

--- ImageCreation/LoadGraph.py
+++ ImageCreation/LoadGraph.py
@@ -1,9 +1,9 @@
 import pickle
 import networkx as nx
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-with open("clusters/graph.pkl", "rb") as f:
+with open("c2/graph.pkl", "rb") as f:
     g = pickle.load(f)
 
-nx.graph(g)
+nx.draw(g, with_labels=False)
 plt.show()
