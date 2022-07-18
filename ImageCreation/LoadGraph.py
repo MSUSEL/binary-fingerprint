10b0471e850e4fde8c4b2e8ed204f3e96a684c7d
RnMor777 <ryan.morganti@trojans.dsu.edu>
2022-07-18 11:35:21 -0600

    Updated graph loading script

--- ImageCreation/LoadGraph.py
+++ ImageCreation/LoadGraph.py
@@ -1,9 +1,17 @@
 import pickle
+import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
 
-with open("c2/graph.pkl", "rb") as f:
-    g = pickle.load(f)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='View graph from pickle')
+    parser.add_argument("-f", "--file", dest='file', type=str, required=True,
+            help='pickled list file')
 
-nx.draw(g, with_labels=False)
-plt.show()
+    args = parser.parse_args()
+
+    with open(args.file, "rb") as f:
+        g = pickle.load(f)
+
+    nx.draw(g, with_labels=False)
+    plt.show()
