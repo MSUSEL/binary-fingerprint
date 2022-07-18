b7d098053212f2829d1d58c2a3ee8e08100a02c7
RnMor777 <ryan.morganti@trojans.dsu.edu>
2022-07-18 15:47:09 -0600

    Created script to restore file from an image

---
+++ ImageCreation/RestoreFromImg.py
@@ -0,0 +1,26 @@
+import argparse
+import numpy as np
+from PIL import Image as im
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Restore file from image')
+    parser.add_argument('-f', '--file', dest='file', type=str, required=True,
+            help='Image file to load to exe')
+    parser.add_argument('-s', '--size', dest='size', type=int, required=True,
+            help='Original size of image')
+    parser.add_argument('-o', '--out', dest='out', type=str, default='out',
+            help='Output destination')
+
+    args = parser.parse_args()
+
+    with open(args.out, "wb") as f:
+        count = 0
+        for i in np.array(im.open(args.file).getdata(), dtype=np.uint8):
+            try:
+                f.write(int(256-i).to_bytes(1, 'big'))
+            except Exception:
+                f.write(int(i).to_bytes(1, 'big'))
+
+            count += 1
+            if count >= args.size:
+                break
